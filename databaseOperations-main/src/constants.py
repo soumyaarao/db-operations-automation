e372e3b426dd04e1ede1ff37c6c542cd4b15d647
Soumya Rao <148032127+soumyaarao@users.noreply.github.com>
2024-01-25 23:54:57 +0530

    Refactored Code

--- databaseOperations-main/src/constants.py
+++ databaseOperations-main/src/constants.py
@@ -1,4 +1,26 @@
+# DB_HOST = "localhost"
+# DB_PORT = "5432"
+# DB_NAME = "operations_db"
+# DB_USER = "soumyarao"
+# DB_PASSWORD = "mypassword"
+#
+#
+# # constants.py
+import configparser
+import os
 
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "operations_db"
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+config_file_path = os.path.join(script_dir, 'config.ini')
+
+print(f"Config file path: {config_file_path}")
+
+# Read the configuration file
+config = configparser.ConfigParser()
+config.read(config_file_path)
+
+DB_HOST = config.get('database', 'host')
+DB_PORT = config.get('database', 'port')
+DB_NAME = config.get('database', 'name')
+DB_USER = config.get('database', 'user')
+DB_PASSWORD = config.get('database', 'password')
--- databaseOperations-main/test/test_dml_ddl_operations.py
+++ databaseOperations-main/test/test_dml_ddl_operations.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from src.db_operations import DatabaseOperations, generate_random_string
-from src.constants import DB_HOST, DB_PORT, DB_NAME
+from src.constants import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 from src.main import init_db
 
 
@@ -9,12 +9,13 @@ class TestDataBaseOperations(TestCase):
     def setUp(self):
         init_db('operations_db', '../database.sql')
         connection_args = {
-            "host" : DB_HOST,
-            "port" : DB_PORT,
-            "database" : DB_NAME,
+            "host": DB_HOST,
+            "port": DB_PORT,
+            "database": DB_NAME,
+            "user": DB_USER,
+            "password": DB_PASSWORD
         }
         self.db_connection = DatabaseOperations(connection_args)
-
 
     def test_dml_operations(self):
         name = generate_random_string()
@@ -30,7 +31,7 @@ class TestDataBaseOperations(TestCase):
         assert len(result) == 1
         assert result[0][1] == name
         assert result[0][2] == email
-
+        self.db_connection.close_connection()
 
     def test_ddl_operations(self):
         create_table_query = """
@@ -46,3 +47,29 @@ class TestDataBaseOperations(TestCase):
         table_exists_query = "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'product')"
         result = self.db_connection.fetch_data(table_exists_query)
         assert result[0][0] is True
+
+        # ALTER TABLE
+        alter_table_query = """
+           ALTER TABLE product
+           ADD COLUMN quantity INT;
+        """
+        self.db_connection.execute_query(alter_table_query)
+
+        # Validation: Check if the 'quantity' column has been added
+        column_exists_query = """
+            SELECT EXISTS (
+                SELECT column_name
+                FROM information_schema.columns
+                WHERE table_name = 'product' AND column_name = 'quantity'
+            )
+        """
+        result = self.db_connection.fetch_data(column_exists_query)
+        assert result[0][0] is True
+
+        # Remove the 'quantity' column if it exists
+        remove_column_query = """
+                    ALTER TABLE product
+                    DROP COLUMN quantity;
+                """
+        self.db_connection.execute_query(remove_column_query)
+        self.db_connection.close_connection()
